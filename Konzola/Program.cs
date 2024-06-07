8ad72183e969e6d240f6a84fdc49cde74c2b0ed4
ddunger <davordunger@gmail.com>
2024-06-07 19:31:04 +0200

    Add project files.

---
+++ Konzola/Program.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using Model;
+
+namespace Konzola
+{
+	internal class Program
+	{
+		static void Main(string[] args)
+		{
+			List<Ucenik> popisUcenika = new List<Ucenik>();
+
+			for (int i = 0; i < 3; i++)
+			{
+				Ucenik noviUcenik = DodajUcenika();
+				if (noviUcenik != null)
+				{
+					popisUcenika.Add(noviUcenik);
+				}
+			}
+
+			foreach (var ucenik in popisUcenika)
+			{
+				string ocjena = ucenik.IspisProsjeka();
+				Console.WriteLine($"\n{ucenik.Ime} {ucenik.Prezime}, Starost: {ucenik.Starost()} godina, Prosjek: {ocjena}");
+			}
+
+			Console.ReadLine();
+		}
+
+		static Ucenik DodajUcenika()
+		{
+			Console.Write("\nUnesite ime ucenika:");
+			string ime = Console.ReadLine();
+
+			Console.Write("Unesite prezime ucenika:");
+			string prezime = Console.ReadLine();
+
+			Console.Write("Unesite datum rodjenja u formatu yyyy-MM-dd:");
+			string dateInput = Console.ReadLine();
+
+			DateTime datumRodjenja = new DateTime(1900, 1, 1);
+
+			if (DateTime.TryParse(dateInput, out DateTime date))
+			{
+				datumRodjenja = DateTime.Parse(dateInput);
+			}
+			else
+			{
+				Console.WriteLine("Datum nije unesen u pravilnom formatu");
+			}
+
+			Console.Write("Unesite prosjek ucenika:");
+			double prosjek = double.Parse(Console.ReadLine());
+
+			if (prosjek < 1.00 || prosjek > 5.01)
+			{
+				Console.Write("Prosjek mora biti izmedju 1 i 5");
+			}
+
+			return new Ucenik(ime, prezime, datumRodjenja, prosjek);
+		}
+	}
+}
---
+++ Ucenik.cs
@@ -0,0 +1,71 @@
+﻿using System;
+
+namespace Model
+
+
+{
+	public class Ucenik
+	{
+		public string Ime { get; set; }
+		public string Prezime { get; set; }
+		public DateTime DatumRodjenja { get; set; }
+		public double Prosjek { get; set; }
+
+
+		public Ucenik(string ime, string prezime, DateTime datumRodjenja, double prosjek)
+		{
+			Ime = ime;
+			Prezime = prezime;
+			DatumRodjenja = datumRodjenja;
+			Prosjek = Math.Round(prosjek, 2);
+		}
+
+
+		public int Starost()
+		{
+			DateTime today = DateTime.Now;
+			int years = today.Year - DatumRodjenja.Year;
+
+			if (DatumRodjenja.Month > today.Month)
+			{
+				years--;
+			}
+			return years;
+		}
+
+		public string IspisProsjeka()
+		{
+			string ispis = string.Empty;
+
+			if (Prosjek >= 1.00 && Prosjek <= 1.49)
+			{
+				ispis = "nedovoljan";
+			}
+			else if (Prosjek >= 1.50 && Prosjek <= 2.49)
+			{
+				ispis = "dovoljan";
+			}
+			else if (Prosjek >= 2.50 && Prosjek <= 3.49)
+			{
+				ispis = "dobar";
+			}
+			else if (Prosjek >= 3.50 && Prosjek <= 4.49)
+			{
+				ispis = "vrlo dobar";
+			}
+			else if (Prosjek >= 4.50 && Prosjek <= 5.00)
+			{
+				ispis = "odličan";
+			}
+			else
+			{
+				ispis = $"Došlo je do pogreške, unijeli ste prosjek {Prosjek}.";
+			}
+
+			return ispis;
+
+		}
+	}
+
+
+}
